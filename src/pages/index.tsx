3eec5e7512232a76dc0a921db9bec64f76d36d7d
chane81 <chane81@naver.com>
2021-08-29 01:52:54 +0900

    modify style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,10 @@ import { Button } from '../components/Button';
 import env from '../../env';
 import Image from 'next/image';
 
+const BtnA = styled.button`
+  ${tw`border-2 border-blue-300 p-2`}
+`;
+
 const Home = () => {
   const store = useStore();
   const { fooModel, barModel } = store;
@@ -23,9 +27,13 @@ const Home = () => {
 
   return (
     <>
-      <div tw='md:flex m-12 bg-gray-100 rounded-xl p-8 md:p-0'>
+      <div>
+        <button className='btn74'>test</button>
+        <BtnA>11</BtnA>
+      </div>
+      <div className='md:flex m-12 bg-gray-100 rounded-xl p-8 md:p-0'>
         <img
-          tw='md:w-96 border-2 sm:border-red-600 md:border-blue-600 md:w-36 md:h-56 sm:w-64 sm:h-80 lg:ml-0 lg:rounded-l-2xl md:rounded-none rounded-full mx-auto'
+          className='md:w-96 border-2 md:w-36 md:h-56 sm:w-64 sm:h-80 lg:ml-0 lg:rounded-l-2xl md:rounded-none rounded-full mx-auto'
           src='https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg'
         />
         <div tw='pt-6 md:flex-1 md:p-8 text-center md:text-left space-y-4'>
@@ -36,23 +44,27 @@ const Home = () => {
               size is tiny.”
             </p>
           </blockquote>
-          <div tw='font-medium'>
+          <div className='font-medium'>
             <div tw='text-blue-600'>Sarah Dayan</div>
             <div tw='text-gray-500'>Staff Engineer, Algolia</div>
           </div>
         </div>
       </div>
-      <div tw='md:flex m-12 bg-gray-100 rounded-xl p-8 md:p-0'>
-        <Image
-          tw='md:border-2 sm:border-red-600 md:border-blue-600 md:w-36 md:h-56 sm:w-64 sm:h-80 lg:ml-0 lg:rounded-l-2xl md:rounded-none rounded-full mx-auto'
-          src='https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg'
-          alt=''
-          className='border-2 border-red-600'
-          objectFit='cover'
-          width='100'
-          height='100'
-        />
-        <div tw='pt-6 md:flex-1 md:p-8 text-center md:text-left space-y-4'>
+      <div className='flex md:flex-row m-12 bg-gray-100 rounded-xl md:p-0 sm:p-4 sm:flex-col'>
+        <div className='md:w-36 sm:w-60 md:h-60 sm:h-72 sm:mx-auto rounded-l-2xl relative'>
+          <Image
+            src='https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg'
+            alt=''
+            className='lg:rounded-l-2xl md:rounded-l-2xl md:rounded-r-none sm:rounded-full'
+            layout='fill'
+            objectFit='fill'
+          />
+        </div>
+        <div
+          tw='pt-6 
+          flex-1
+          md:p-8 text-center md:text-left space-y-4'
+        >
           <blockquote>
             <p tw='text-lg font-semibold'>
               “Tailwind CSS is the only framework that I've seen scale on large
