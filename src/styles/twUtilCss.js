e37d223d6026c0e27340162476d573fd6119d1a8
chane81 <chane81@naver.com>
2021-08-28 23:26:35 +0900

    add tailwind util class

---
+++ src/styles/twUtilCss.js
@@ -0,0 +1,16 @@
+/** @typedef { import('@emotion/css').CSSObject } css */
+
+/** @type { css } */
+const twUtilCss = {
+  '.btn23': {
+    padding: '.5rem 1rem',
+    borderRadius: '.25rem',
+    border: '1px',
+    borderStyle: 'solid',
+    fontWeight: '600',
+    color: 'yellow',
+    borderColor: 'blue'
+  }
+};
+
+module.exports = twUtilCss;
