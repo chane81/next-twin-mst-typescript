5fe1fa5ceba85338782389fbef7c8cd918a41655
chane81 <chane81@naver.com>
2021-08-28 23:25:25 +0900

    modify config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
+const twUtilCss = require('./src/styles/twUtilCss')
+
 module.exports = {
+  mode: 'jit',
+  dark: false,
+  purge: [
+    // Your CSS will rebuild any time *any* file in `src` changes
+    './src/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -7,5 +15,11 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities, addComponents, e, prefix, config }) {
+
+      addUtilities(twUtilCss);
+
+    }
+  ],
 }
